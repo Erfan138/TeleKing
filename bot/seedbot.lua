38837fd5a74d9827e8aea77e7d7b3a766c758d90
Erfan138 <erfan.graphic@gmail.com>
2016-03-11 12:07:07 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -225,14 +225,13 @@ function create_config( )
     "set",
     "get",
     "broadcast",
-    "invite",
     "all",
     "leave_ban",
 	"supergroup",
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {110626080,103649648,111020322,0,tonumber(193229838)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
